e2bd9f6c67e6e8d2519b331b40a49d154875b186
hsuanPRHR <105162870+hsuanPRHR@users.noreply.github.com>
2022-05-08 22:57:56 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/KF6hmdnrxKM56TLP6',
+	'https://forms.gle/Kr8DoZFsK7TsNVTo8'
 ];
